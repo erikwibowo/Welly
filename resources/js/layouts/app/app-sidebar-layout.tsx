8203f71da1f9025a2852ea4061b9f8cbd5991c03
Erik Wibowo <erikwibo@gmail.com>
2025-05-28 10:19:16 +0700

    refactor: restructure AppSidebarLayout to improve layout and component organization

--- resources/js/layouts/app/app-sidebar-layout.tsx
+++ resources/js/layouts/app/app-sidebar-layout.tsx
@@ -1,20 +1,19 @@
-import { AppContent } from '@/components/app-content';
 import { AppShell } from '@/components/app-shell';
 import { AppSidebarHeader } from '@/components/app-sidebar-header';
-import { BottomNavbar } from '@/components/bottom-navbar';
 import { type BreadcrumbItem } from '@/types';
 import { type PropsWithChildren } from 'react';
 import { AppSidebar } from '../authenticated/app-sidebar';
+import { BottomNavbar } from '@/components/bottom-navbar';
 
 export default function AppSidebarLayout({ children, breadcrumbs = [] }: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
     return (
         <AppShell variant="sidebar">
             <AppSidebar />
-            <AppContent variant="sidebar">
+            <div className="min-h[100svh] grid flex-1 grid-rows-[auto_1fr_auto]">
                 <AppSidebarHeader breadcrumbs={breadcrumbs} />
-                {children}
+                <div className="overflow-x-hidden overflow-y-auto">{children}</div>
                 <BottomNavbar />
-            </AppContent>
+            </div>
         </AppShell>
     );
 }
